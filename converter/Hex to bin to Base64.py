62ae9ae8ed29a389537ec51337aa9fd4131f9242
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-11 16:06:15 +0300

    upload

--- converter/Hex to bin to Base64.py
+++ converter/Hex to bin to Base64.py
@@ -1,8 +1,8 @@
 ##Hex to bin and bin to base64
 import binascii
 import base64
-HexDecimal = str(raw_input("Enter HexDecimal number ONLY: "))
+HexDecimal = str(input("Enter HexDecimal number ONLY: "))
 var1 = binascii.a2b_hex(HexDecimal)
-print var1
-print base64.b64encode(var1)
+print (var1)
+print (base64.b64encode(var1))
 
